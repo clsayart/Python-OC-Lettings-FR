3b1d39de49e2f218c17fb34ba6cae5e2def64a8f
SAYART Claire <claire.sayart@teamwork.net>
2023-06-01 10:28:31 +0200

    Procfile - sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -109,3 +111,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+sentry_sdk.init(
+    # METTRE VARIABLE SENTRY LA CREER DANS CIRCLE CI /ET HEROKU???
+    dsn="https://dff69f8b0c734ee49f835dcc5f8c9cbd@o4505272157929472.ingest.sentry.io/4505272224317440",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sentry-debug/', trigger_error),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
