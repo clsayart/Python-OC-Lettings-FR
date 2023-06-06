6db9018f068c5eb21af6679378a3f103b87ebb32
SAYART Claire <claire.sayart@teamwork.net>
2023-06-06 14:23:09 +0200

    psycog2

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,5 @@ RUN pip install -r requirements.txt
 COPY . /app
 
 EXPOSE $PORT
-RUN python manage.py collectstatic --noinput
 CMD python manage.py runserver 0.0.0.0:$PORT
 # CMD ["python", "./manage.py", "runserver", "0.0.0.0:8080"]
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,7 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,11 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    "./static/",
-]
+django_heroku.settings(locals())
 
 sentry_sdk.init(
     # METTRE VARIABLE SENTRY LA CREER DANS CIRCLE CI /ET HEROKU???
