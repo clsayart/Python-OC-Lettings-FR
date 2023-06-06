87c605ab1989212674e4c65cb270bf292238ae67
SAYART Claire <claire.sayart@teamwork.net>
2023-06-06 14:08:37 +0200

    static root

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -111,6 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    "./static/",
+]
 
 sentry_sdk.init(
     # METTRE VARIABLE SENTRY LA CREER DANS CIRCLE CI /ET HEROKU???
